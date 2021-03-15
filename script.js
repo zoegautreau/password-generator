c453c580e12ea5cdac6e4a51ce50098f7e1d536c
Zoe Gautreau <gautreauzoe@gmail.com>
2021-03-15 16:02:41 -0300

    added variables

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var amountPrompt = prompt("How many characters would you like your password to have? *Must be more than 8, and less than 128*");
+
+var lowercaseConfirm = confirm("Would you like lowercase letters in your password? Select 'OK' for YES and 'Cancel' for NO");
+
+var uppercaseConfirm = confirm("Would you like uppercase letters in your password? Select 'OK' for YES and 'Cancel' for NO");
+
+var specialConfirm = confirm("Would you like special characters in your password? Select 'OK' for YES and 'Cancel' for NO");
+
+var lowercaseConfirm = confirm("Would you like numbers in your password? Select 'OK' for YES and 'Cancel' for NO");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,15 +24,5 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-  prompt("How many characters would you like your password to have? ");
-
-prompt("Would you like lowercase letters in your password? Select 'OK' for YES and 'Cancel' for NO")
-
-prompt("Would you like uppercase letters in your password? Select 'OK' for YES and 'Cancel' for NO")
-
-prompt("Would you like special characters in your password? Select 'OK' for YES and 'Cancel' for NO")
-
-prompt("Would you like numbers in your password? Select 'OK' for YES and 'Cancel' for NO")
-
-}
+generateBtn.addEventListener("click", writePassword);
+  
