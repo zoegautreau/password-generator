e832a7c959667dc5bc115db6ea50e68806e7e317
Zoe Gautreau <gautreauzoe@gmail.com>
2021-03-16 20:50:41 -0300

    testing things

--- script.js
+++ script.js
@@ -11,26 +11,79 @@ var specialConfirm = confirm("Would you like special characters in your password
 
 var numbersConfirm = confirm("Would you like numbers in your password? Select 'OK' for YES and 'Cancel' for NO");
 
+// Prompt for amount of characters (number value input)
 function checkAmount() {
   amountPrompt;
 }
 
+// Lowercase confirmation (true or false input - OK or Cancel)
 function checkLowercase() {
   lowercaseConfirm;
 }
 
+// Uppercase confirmation (true or false - OK or Cancel)
 function checkUppercase() {
   uppercaseConfirm;
 }
 
+// Special characters confirmation (true or false - OK or Cancel)
 function checkSpecial() {
   specialConfirm;
 }
 
+// Numbers confirmation (true or false - OK or Cancel)
 function checkNumbers() {
   numbersConfirm;
 }
 
+// Checks if user wants lowercase letters (true or false input - OK or Cancel)
+if (checkLowercase) {
+
+  else {
+
+  }
+}
+
+// Checks if user inputs a number lower than 8 or higher than 128
+if (checkAmount < 8 || checkAmount > 128){
+  alert("Length must be 8-128 characters")
+}
+
+
+if (checkAmount >= 8 && checkAmount <= 128){
+  var uppercaseConfirm = confirm("Would you like to use uppercase letters?")
+  var lowercaseConfirm = confirm("Would you like to use lowercase letters?")
+  var numbersConfirm = confirm("would you like to use numbers?")
+  var specialConfirm = confirm("would you like to use special characters?")
+  }    
+  
+  if (uppercaseConfirm != true && lowercaseConfirm != true && numbersConfirm != true && specialConfirm != true){
+      alert("You must select at least one character type!")
+  }
+
+// Checks if user wants uppercase letters (true or false input - OK or Cancel)
+if (checkUppercase) {
+
+  else {
+
+  }
+}
+
+// Checks if user wants special characters (true or false input - OK or Cancel)
+if (checkSpecial) {
+
+  else {
+
+  }
+}
+
+// Checks if user wants numbers (true or false input - OK or Cancel)
+if (checkNumbers) {
+
+  else {
+
+  }
+}
 
 
 
@@ -43,9 +96,24 @@ function writePassword() {
 
 }
 
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
   
+// Click button
+
+var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var uppercaseOnly = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseOnly = "abcdefghijklmnopqrstuvwxyz";
+var numbersOnly = "0123456789";
+var specialOnly = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var upperLowerNumbers = "ABCDEFGHHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+var upperLowerSpecial = "ABCDEFGHHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var upperNumbersSpecial = "ABCDEFGHHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var lowerNumbersSpecial = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var upperLower = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var upperNumbers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+var upperSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var lowerNumbers = "abcdefghijklmnopqrstuvwxyz0123456789";
+var lowerSpecial = "abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var numbersSpecial = "0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
